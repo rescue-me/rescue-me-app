5c67e76e39f1f0ae50680f2d39b38e8e541fcbf5
AlejandroBudy <atorresato@gmail.com>
2022-01-05 13:18:56 +0100

    Extracted in typealias result

--- src/main/kotlin/rescueme/com/modules/dog/Service.kt
+++ src/main/kotlin/rescueme/com/modules/dog/Service.kt
@@ -5,19 +5,21 @@ import arrow.core.computations.either
 import rescueme.com.modules.shared.Has
 import java.util.*
 
-suspend fun <R> R.save(dog: Dog): Either<Throwable, Dog> where R : Has.DogRepository, R : Has.NotificationRepository =
+typealias DogOutcome<R> = Either<Throwable, R>
+
+suspend fun <R> R.save(dog: Dog): DogOutcome<Dog> where R : Has.DogRepository, R : Has.NotificationRepository =
     either {
         val dogSaved = repository.save(dog)
         notificationRepository.publish(DogCreated.apply(dogSaved))
         dogSaved
     }
 
-suspend fun <R> R.getAll(): Either<Throwable, List<Dog>> where R : Has.DogRepository =
+suspend fun <R> R.getAll(): DogOutcome<List<Dog>> where R : Has.DogRepository =
     either {
         repository.getAll()
     }
 
-suspend fun <R> R.getByShelter(shelter: String): Either<Throwable, List<Dog>> where R : Has.DogRepository =
+suspend fun <R> R.getByShelter(shelter: String): DogOutcome<List<Dog>> where R : Has.DogRepository =
     either {
         val shelterId = UUID.fromString(shelter)
         repository.getByShelter(shelterId)
