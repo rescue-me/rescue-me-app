0dc2259f3bbe2022b9afc88bfec50cd7bbfc924e
AlejandroBudy <atorresato@gmail.com>
2022-01-22 17:47:27 +0100

    User bind post test implementation

--- src/test/kotlin/rescueme/com/modules/users/UsersCoreTest.kt
+++ src/test/kotlin/rescueme/com/modules/users/UsersCoreTest.kt
@@ -4,15 +4,22 @@ import arrow.core.None
 import arrow.core.left
 import arrow.core.right
 import arrow.core.toOption
+import io.kotest.core.spec.IsolationMode
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.mockk
+import io.mockk.verify
 import rescueme.com.modules.shared.BadRequest
+import rescueme.com.modules.shared.NotificationRepository
+import rescueme.com.modules.shared.RepositoryGenericException
 import rescueme.com.modules.shared.ResourceNotFoundException
 import java.util.*
 
 class UsersCoreTest : FunSpec({
+
+    isolationMode = IsolationMode.InstancePerTest
 
     val uuidString = "5f42a79c-6579-43ff-959d-c887d9738523"
     val uuid = UUID.fromString("5f42a79c-6579-43ff-959d-c887d9738523")
@@ -20,6 +27,7 @@ class UsersCoreTest : FunSpec({
 
     val context = object : Context {
         override val repository: Repository = mockk()
+        override val notificationRepository: NotificationRepository = mockk()
     }
 
     context("Retrieving users") {
@@ -42,6 +50,24 @@ class UsersCoreTest : FunSpec({
             val actual = context.bindGet(uuidString)
 
             actual shouldBe ResourceNotFoundException("User with uuid $uuidString not found").left()
+        }
+    }
+
+    context("Creating a new user") {
+        test("Should create a new user and publish message") {
+            val userCreated = UserCreated(user.id)
+            coEvery { context.repository.save(user) } returns user.right()
+            coEvery { context.notificationRepository.publish(userCreated) } returns Unit
+
+            context.bindPost(uuidString) shouldBe user.right()
+        }
+
+        test("Should return left with DomainException"){
+            coEvery { context.repository.save(user) } returns RepositoryGenericException().left()
+
+            context.bindPost(uuidString) shouldBe RepositoryGenericException().left()
+
+            coVerify (exactly = 0) { context.notificationRepository.publish(any())  }
         }
     }
 })
