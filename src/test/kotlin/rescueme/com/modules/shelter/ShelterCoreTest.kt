64c196ef914dc20290f992058006bb4980c3b9b7
AlejandroBudy <atorresato@gmail.com>
2022-02-15 20:26:03 +0100

    Spec for shelter core

---
+++ src/test/kotlin/rescueme/com/modules/shelter/ShelterCoreTest.kt
@@ -0,0 +1,18 @@
+package rescueme.com.modules.shelter
+
+import io.kotest.core.spec.style.FunSpec
+
+class ShelterCoreTest : FunSpec({
+
+    context("Create new shelter") {
+
+    }
+
+    context("Retrieve all shelters") {
+
+    }
+
+    context("Retrieve shelter by id") {
+
+    }
+})
